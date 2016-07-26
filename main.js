81c5d85a254618c79c6a198e5ffbd832b4c8a31b
Angus Trau <angus.amtra5@gmail.com>
2016-07-26 19:26:05 +1000

    Added base for send, image, and receiving

--- main.js
+++ main.js
@@ -1,1 +1,64 @@
 ﻿const hughbot = module.parent.exports;
+const login = require("facebook-chat-api");
+const request = require("request");
+const stream = require("stream");
+
+class FBMessenger extends hughbot.Adapter {
+    constructor(robot) {
+        super(robot, "FBMessenger");
+    }
+
+    connect() {
+        login({
+            email: process.env.HUGHBOT_FBMESSENGER_EMAIL,
+            password: process.env.HUGHBOT_FBMESSENGER_PASSWORD
+        }, (err, api) => {
+            if (err) {
+                this.robot.emit("error", "Error logging in to Facebook Messenger:");
+                this.robot.emit("error", err);
+                return;
+            }
+
+            api.setOptions({ listenEvents: true })
+
+            this.send = (message) => {
+                api.sendMessage({ body: message.message }, message.room);
+            }
+
+            this.image = (message) => {
+                let image = new stream.PassThrough();
+
+                request(message.imageURL).pipe(image)
+                api.sendMessage({ attatchment: image }, message.room);
+            }
+
+            api.listen((err, e) => {
+                if (err) {
+                    this.robot.emit("error", e);
+                    return;
+                }
+
+                switch (e.type) {
+                    case "message":
+                        break;
+                    case "event":
+                        break;
+                }
+            });
+        });
+    }
+}
+
+module.exports = (robot) => {
+    if (!process.env.HUGHBOT_FBMESSENGER_EMAIL) {
+        robot.emit("error", new Error("Set HUGHBOT_FBMESSENGER_EMAIL"));
+        process.exit();
+    }
+
+    if (!process.env.HUGHBOT_FBMESSENGER_PASSWORD) {
+        robot.emit("error", new Error("Set HUGHBOT_FBMESSENGER_PASSWORD"));
+        process.exit();
+    }
+
+    robot.addAdapter(new FBMessenger(robot));
+}
